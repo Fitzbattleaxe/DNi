76e32a11a7180c753e8a570a3d6d9e47d844d0c1
Dane Wallinga <danewallinga@gmail.com>
2017-01-08 14:18:05 -0800

    Start thinking about how to beef up alarms.

---
+++ app/src/main/java/com/dane/dni/alarms/external/CustomAlarmReceiverDeluxe.java
@@ -0,0 +1,16 @@
+package com.dane.dni.alarms.external;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Dane on 1/8/2017.
+ */
+
+public class CustomAlarmReceiverDeluxe extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/dane/dni/alarms/handler/AlarmHandler.java
@@ -0,0 +1,15 @@
+package com.dane.dni.alarms.handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dane.dni.R;
+
+public class AlarmHandler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_handler);
+    }
+}
